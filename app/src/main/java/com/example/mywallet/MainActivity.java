fee3a9daccf40af0a96c8b200f0851649fb9bbe3
Cuong <dlvcuong.1711@gmail.com>
2026-05-22 20:58:08 +0700

    fix and update

--- app/src/main/java/com/example/mywallet/MainActivity.java
+++ app/src/main/java/com/example/mywallet/MainActivity.java
@@ -14,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = findViewById(R.id.bottomNavigation);
